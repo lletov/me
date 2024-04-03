addf85523a7b23a5a311fb7b22e3519d9a0770e7
LetovD <dmitry2595@gmail.com>
2024-04-03 12:37:34 +0300

    feat: add open-meteo api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { Weather } from './components/Weather';
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +19,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Weather/>
         </a>
       </header>
     </div>

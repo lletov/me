4200c360bd37a993a7c391e5b887130d89ae3731
Dmitry <dmitry2595@gmail.com>
2024-04-15 17:23:04 +0300

    feat: style editing

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,19 +4,19 @@ export const Experience = () => {
   return (
     <div className='content'>
       <h2>Experience <span className='tag'></span></h2>
-      <p className='orange'><strong>Frontend developer</strong> (2022 -&gt; current)</p>
+      <p className='orange'>Frontend developer (2022 -&gt; current)</p>
       <h4>"M***"</h4>
-      <p className='mr-left-24'>- Implemented and supported the operation of a web application for organizing production work with the team. It includes working with the API, loading and working with images (we used canvas). Also a user guide, installation instructions, and interface tests developed.</p>
-      <h4 className='mr-left-24 green'>Stack: Vue 3 + Router, WEB API, JS, HTML, SCSS, GitLab, Docker, Figma</h4>
+      <h3 className='mr-left-24'>&gt;&gt;&gt; Implemented and supported the operation of a web application for organizing production work with the team. It includes working with the API, loading and working with images (we used canvas). Also a user guide, installation instructions, and interface tests developed.</h3>
+      <h3 className='mr-left-24'><span className='orange'>Stack:</span> Vue 3 + Router, WEB API, JS, HTML, SCSS, GitLab, Docker, Figma</h3>
       <br/>
-      <p className='mr-left-24'>- Implemented an application for the customer with the team, with the ability to broadcast online (via WebSocket) & work with objects on maps (we used a leaflet)</p>
-      <h4 className='mr-left-24 green'>Stack: Vue 3, VUEX, WEB API, JavaScript, HTML, CSS, GitLab, Docker, Figma</h4>
+      <h3 className='mr-left-24'>&gt;&gt;&gt; Implemented an application for the customer with the team, with the ability to broadcast online (via WebSocket) & work with objects on maps (we used a leaflet)</h3>
+      <h3 className='mr-left-24'><span className='orange'>Stack:</span> Vue 3, VUEX, WEB API, JavaScript, HTML, CSS, GitLab, Docker, Figma</h3>
       <br/>
       <br/>
       <p className='orange'><strong>Engineer / Technical writer</strong> (2016 -&gt; 2023)</p>
       <h4>JSC "N***"</h4>
-      <p className='mr-left-24'>- led one of the development areas (requirements described, technical documentation developed, performers communicated, products production controlled and tested)</p>
-      <h4 className='mr-left-24 green'>Stack: MS Office, AutoCAD</h4>
+      <h3 className='mr-left-24'>&gt;&gt;&gt; led one of the development areas (requirements described, technical documentation developed, performers communicated, products production controlled and tested)</h3>
+      <h3 className='mr-left-24'><span className='orange'>Stack:</span> MS Office, AutoCAD</h3>
     </div>
   )
 }
